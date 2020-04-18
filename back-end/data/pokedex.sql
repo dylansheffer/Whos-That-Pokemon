0d066f40a1327e9ed66a136f5fa9fc261aedfe8a
Dylan Sheffer <dsheffer16@gmail.com>
2020-04-18 17:10:38 -0400

    feat: docker refactor and db seed

--- back-end/data/pokedex.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS "pokedex";
-
-CREATE TABLE IF NOT EXISTS "pokedex" (
-"pokedex_id" SERIAL PRIMARY KEY,
-"user_id" INT NOT NULL
-CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES users ("user_id")
-);
---
+++ back-end/db/init/00-database.sql
@@ -0,0 +1,43 @@
+\connect pokemon;
+
+/* Create user table */
+DROP TABLE IF EXISTS "users";
+
+CREATE TABLE IF NOT EXISTS "users" (
+"user_id" SERIAL PRIMARY KEY,
+"username" TEXT NULL,
+"first_name" TEXT NULL,
+"last_name" TEXT NULL
+);
+
+/* Create pokemon table */
+DROP TABLE IF EXISTS "pokemon";
+
+CREATE TABLE IF NOT EXISTS "pokemon" (
+"pokemon_id" INT PRIMARY KEY,
+"name" TEXT NOT NULL,
+"image" TEXT NOT NULL,
+UNIQUE ("pokemon_id", "name")
+);
+
+/* Create pokedex table */
+DROP TABLE IF EXISTS "pokedex";
+
+CREATE TABLE IF NOT EXISTS "pokedex" (
+"pokedex_id" SERIAL PRIMARY KEY,
+"user_id" INT NOT NULL,
+FOREIGN KEY ("user_id") REFERENCES users ("user_id")
+);
+
+/* Create pokedex-entries table */
+DROP TABLE IF EXISTS "pokedex_entries";
+
+CREATE TABLE IF NOT EXISTS "pokedex_entries" (
+"pokedex_id" INT NOT NULL,
+"pokemon_id" INT NOT NULL,
+"seen" BOOL DEFAULT false,
+"caught" BOOL DEFAULT false,
+PRIMARY KEY ("pokedex_id", "pokemon_id"),
+FOREIGN KEY ("pokedex_id") REFERENCES pokedex ("pokedex_id") ON DELETE CASCADE,
+FOREIGN KEY ("pokemon_id") REFERENCES pokemon ("pokemon_id") ON DELETE CASCADE
+);
