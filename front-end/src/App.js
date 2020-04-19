368f6d5f14d1a7326d73666769813cf841007cdd
Dylan Sheffer <dsheffer16@gmail.com>
2020-04-19 16:47:41 -0400

    feat: connect react to graphql api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
 import './App.css';
 
+const POKEDEX_ENTRIES = gql`
+  {
+    pokedexEntry(pokedexId: 1, pokemonId: 104) {
+      caught
+      seen
+      pokemon {
+        name
+        pokemonId
+        image
+      }
+    }
+  }
+`
 function App() {
+  const { loading, error, data } = useQuery(POKEDEX_ENTRIES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={data.pokedexEntry.pokemon.image} className="App-logo" alt="logo" />
+        <h1>
+          {data.pokedexEntry.pokemon.name}
+        </h1>
       </header>
     </div>
   );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5433/graphql',
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
