671a68b8f6896019e5691a676539e582145bafbd
Dylan Sheffer <dsheffer16@gmail.com>
2020-04-19 16:52:41 -0400

    refactor: use destructuring

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -19,15 +19,17 @@ const POKEDEX_ENTRIES = gql`
 `
 function App() {
   const { loading, error, data } = useQuery(POKEDEX_ENTRIES);
+
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  console.log(data);
+  if (error) return <p>Error <span role="img" aria-label="scream">😱</span></p>;
+
+  const { pokedexEntry: { pokemon } } = data;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={data.pokedexEntry.pokemon.image} className="App-logo" alt="logo" />
+        <img src={pokemon.image} className="App-logo" alt="logo" />
         <h1>
-          {data.pokedexEntry.pokemon.name}
+          {pokemon.name}
         </h1>
       </header>
     </div>
